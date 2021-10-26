1559e43c3708663ac3f95c007fa98028db753717
dkay-mktg-data <darius.kay@ruggable.com>
2021-10-26 16:46:02 -0700

    prod changes

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 import os
-from slack_bolt import App
+# from slack_bolt import App
+from slack_bolt.async_app import AsyncApp
 
 from libs.redshift import handle_grant
 
 # Initializes your app with your bot token and signing secret
-app = App(
+app = AsyncApp(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
 @app.command("/grant")
-def repeat_text(ack, respond, command):
+async def repeat_text(ack, respond, command):
     # Acknowledge command request
-    ack()
+    await ack()
     username, *tablename = command['text'].split()
     handled = handle_grant(username, tablename[0])
     if handled:
-        respond(f"You've just been GRANTED.\nI've just PRESERVED the UNION between you and {tablename[0]}.")
+        await respond(f"You've just been GRANTED.\nI've just PRESERVED the UNION between you and {tablename[0]}.")
     else:
-        respond(f"grant {command['text']}? Nope, nope I won't do that.")
+        await respond(f"grant {command['text']}? Nope, nope I won't do that.")
 
 # Start your app
 if __name__ == "__main__":
